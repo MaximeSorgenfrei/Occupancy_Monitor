d03ce9a57e478e61ed8114def105c03e822a9b40
Maxime Philippe Sorgenfrei <m.p.sorgenfrei@googlemail.com>
2024-04-01 00:10:58 +0200

    clean Log

--- src/log/__init__.py
+++ src/log/__init__.py
@@ -8,28 +8,45 @@ class Log():
         self.time_FMT_file = '%Y-%m-%d_%H-%M-%S'
         self.time_FMT_log = '%Y-%m-%d %H:%M:%S (%Z %z)'
         self.time_FMT_event = '%Y-%m-%d %H:%M:%S'
-        self.timestamp,self.timestamp_file = time.strftime(self.time_FMT_event), time.strftime(self.time_FMT_file)
+        self.timestamp,self.timestamp_file = self._get_event_time(), time.strftime(self.time_FMT_file)
         self.log_file = f"log_file_{self.timestamp_file}.log"
-        if os.path.exists(self.path_to_log_folder) is not True:
-            os.mkdir(self.path_to_log_folder)
-            print(f"[{time.strftime(self.time_FMT_event)}]\tLog folder has been created. ({self.path_to_log_folder})")
         
         self.file_handle = open(os.path.join(self.path_to_log_folder,self.log_file),"w")
-        self.file_handle.write(f"[{time.strftime(self.time_FMT_log)}]\tLog file has been created.\n")
-
-    def write_to_log(self,text):
-        if type(text) == str:
-            self.file_handle.write(f"{text}\n")
-        else:
-            raise TypeError(f"'{text}' must be of type {type(str)} not {type(text)}.")
-
-    def log_event(self,occupation):
-        if occupation:
-            self.write_to_log(f"[{time.strftime(self.time_FMT_event)}]\tOccupation of room has been detected.")
-        else:
-            self.write_to_log(f"[{time.strftime(self.time_FMT_event)}]\tNon-occupation of room has been detected.")
+        self.file_handle.write(f"[{self._get_log_time()}]\tLog file has been created.\n")
 
     def __exit__(self):
-        tdelta = datetime.strptime(time.strftime(self.time_FMT_event), self.time_FMT_event) - datetime.strptime(self.timestamp, self.time_FMT_event)
-        self.write_to_log(f"[{time.strftime(self.time_FMT_log)}]\tLog file has been closed. Service has been running for {tdelta} (hh:mm:ss)")
+        self.file_handle.write(f"[{self._get_log_time()}]\tLog file has been closed. Service has been running for {self._get_log_runtime()} (hh:mm:ss)")
         self.file_handle.close()
+
+    def _set_path_to_log_folder(self, path):
+        if os.path.exists(path) is not True:
+            self._create_folder(path)
+        self.path_to_log_folder = path
+
+    def _create_folder(self, folder):
+        os.mkdir(folder)
+        print(f"[{self._get_event_time()}]\tLog folder has been created. ({folder})")
+
+    def _get_event_time(self):
+        return time.strftime(self.time_FMT_event)
+    
+    def _get_log_time(self):
+        return time.strftime(self.time_FMT_log)
+
+    def _get_log_runtime(self):
+        return datetime.strptime(self._get_event_time(), self.time_FMT_event) - datetime.strptime(self.timestamp, self.time_FMT_event)
+
+    def _write_to_log(self,text : str):
+        if type(text) == str:
+            self.file_handle.write(f"[{self._get_event_time()}]\t{text}\n")
+        else:
+            raise TypeError(f"'{text}' must be of type {type(str)} not {type(text)}.")
+    
+    def log(self, message : str):
+        self._write_to_log(message)
+    
+    def log_event(self, occupation : bool):
+        self._write_to_log(f"{self._get_event_text(occupation)}")
+
+    def _get_event_text(self, occupation : bool = False):
+        return "Occupation of room has been detected." if occupation else "Non-occupation of room has been detected."
